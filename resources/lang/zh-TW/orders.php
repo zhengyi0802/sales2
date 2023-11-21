24594913f3a4006fbf11e703891b5c0ba9b65d82
Chevy Lin <chevylin0802@gmail.com>
2023-11-21 16:16:58 +0800

    update

---
+++ resources/lang/zh-TW/orders.php
@@ -0,0 +1,17 @@
+<?php
+
+    return [
+           'title'       => '訂單資料表',
+           'header'      => '訂單資料表',
+           'id'          => '編號',
+           'name'        => '訂貨人',
+           'model'       => '產品型號',
+           'project'     => '行銷方案',
+           'phone'       => '聯絡電話',
+           'address'     => '裝機地址',
+           'sales'       => '經辦人員',
+           'extra'       => '贈品',
+           'status'      => '狀態',
+           'creator'     => '建立者',
+    ];
+
